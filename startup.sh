24168b2c8e1ff5568a5c4fb38a930cda9a4c268d
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-12-01 14:39:03 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #startup script
 
-bash /root/addcronjob.bash
+#bash /root/addcronjob.bash
 
-echo "$BROADCAST_IP" > /verbs/BROADCAST_IP
-echo "$MAC_ADDRESS" > /verbs/MAC_ADDRESS
-if [ -z "$TRIGGER_IP" ]
-then
-	echo "No Trigger IP" >> log.log
-else
-	echo "$TRIGGER_IP" > /verbs/TRIGGER_IP
-fi
-service cron start
+echo "$dnsEntryNames" > /verbs/dnsEntryNames
+echo "$dnsEntryExpire" > /verbs/dnsEntryExpire
+echo "$dnsEntryType" > /verbs/dnsEntryType
+echo "$TransipUsername" > /verbs/TransipUsername
+echo "$WhitelistIPonly" > /verbs/WhitelistIPonly
+echo "$PrivateKey" > /verbs/PrivateKey
+
+#if [ -z "$TRIGGER_IP" ]
+#then
+#	echo "No Trigger IP" >> log.log
+#else
+#	echo "$TRIGGER_IP" > /verbs/TRIGGER_IP
+#fi
+#service cron start
 tail -fn0 log.log
