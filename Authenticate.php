05b6308de23d8ee4995eb20f84500a369a1db412
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-12-01 13:09:23 +0100

    Create Authenticate.php

---
+++ Authenticate.php
@@ -0,0 +1,27 @@
+<?php
+
+use \Transip\Api\Library\TransipAPI;
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+// Your login name on the TransIP website.
+$login = '';
+
+// If the generated token should only be usable by whitelisted IP addresses in your Controlpanel
+$generateWhitelistOnlyTokens = false;
+
+// One of your private keys; these can be requested via your Controlpanel
+$privateKey = '';
+
+$api = new TransipAPI(
+       $login,
+       $privateKey,
+       $generateWhitelistOnlyTokens
+);// Set all generated tokens to read only mode (optional)$api->setReadOnlyMode(false);
+
+// Create a test connection to the api
+//$response = $api->test()->test();
+//
+//if ($response === true) {
+//   echo 'API connection successful!';
+//}
