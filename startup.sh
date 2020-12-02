4e7b8df00ff03aa773b0dbfc69d7f883647c10a9
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-12-02 11:38:30 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,12 +12,15 @@ echo "$WhitelistIPonly" > /verbs/WhitelistIPonly
 echo "$PrivateKey" > /verbs/PrivateKey
 touch /verbs/prev_publicIP
 
+#fix privatekey
 sed -i 's/-----BEGIN PRIVATE KEY----- //' /verbs/PrivateKey
 sed -i 's/ -----END PRIVATE KEY-----//' /verbs/PrivateKey
 tr " " "\n" < /verbs/PrivateKey > /tmp/PrivateKey && mv /tmp/PrivateKey /verbs/PrivateKey
 sed -i '1s/^/-----BEGIN PRIVATE KEY-----\n/' /verbs/PrivateKey
 echo '-----END PRIVATE KEY-----' >> /verbs/PrivateKey
 
+#fix DNS entry names
+tr ' ' '' < /verbs/dnsEntryNames > /tmp/dnsEntryNames && mv /tmp/dnsEntryNames /verbs/dnsEntryNames
 
 #if [ -z "$TRIGGER_IP" ]
 #then
