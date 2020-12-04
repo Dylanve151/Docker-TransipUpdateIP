e0bafb104562d3a3a4b4f6bdfbb90b283c569051
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-12-04 15:34:20 +0100

    Update Certbot-DNS.sh

--- Certbot-DNS.sh
+++ Certbot-DNS.sh
@@ -9,7 +9,7 @@ dnsEntryValue="$CERTBOT_VALIDATION"
 
 cur_dnsEntryValue=""
 
-php updateDNSentry.php "$domainName" "$dnsEntryName" "$dnsEntryExpire" "$dnsEntryType" "$dnsEntryValue"
+php UpdateDNSentry.php "$domainName" "$dnsEntryName" "$dnsEntryExpire" "$dnsEntryType" "$dnsEntryValue"
 
 while [[ "${dnsEntryValue}" != "${cur_dnsEntryValue}" ]]; do
 	cur_dnsEntryValue=$(dig -t txt _acme-challenge.velst.nl +short)
